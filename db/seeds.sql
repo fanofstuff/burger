f437773f588c7ead5bd39db383f36162e929e4fe
Tucker Hembree <bladeofxp@gmail.com>
2020-04-10 18:50:11 -0400

    created basic file structure according to instructions; notably, I'm relying heavily upon the CatsApp that we went over in class at the moment; my next steps are to test the backend stuff that I've modified using Postman and then change the basic Handlebars HTML to match (roughly) the website in the example video

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Crazy Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Wanna-b-Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger"); 
+INSERT INTO burgers (burger_name) VALUES ("Sad Burger"); 
